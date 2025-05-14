6d76c02e9270fbc1132c9a3ba7a1fc6df746f58a
Jumana Haseen <50680796+Jumana07@users.noreply.github.com>
2025-05-14 16:49:29 +0530

    yes

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,8 @@ function About() {
       <p style={{ background: '#a569bd', color: 'white', textAlign: 'center',padding:'0.2rem' }}>
         I'm a frontend developer with a B.E CSE degree <br/>I have a  self practice experience of 2 years in React with frontend development <br/>
         Want to know More,Click here for resume... <br/>
-        <a href= "../resume.pdf" target="_blank">Download</a>
+        <a href="/resume.pdf" target="_blank" rel="noopener noreferrer">Download</a>
+
 </p>
     </section>
   );
